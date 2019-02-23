3232f2809d331913420648f4470bc174a3763569
jcosentino <johnsc301@gmail.com>
2019-02-23 15:23:23 -0500

    [2/23/19] - 3:23pm - added initial database models and code

--- API/app.py
+++ API/app.py
@@ -2,36 +2,78 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from datetime import datetime
+#create_sql =  './Files/create.sql'
 
 app = Flask(__name__)
-CORS(app)
+CORS(app) #Need for access from other applications, such as Axios
+#Should create schema if it doesn't already exist
+#unsure how to do this
+#
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:8milerun@localhost/testdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Turn off annoying message
 db = SQLAlchemy(app)
 
-#Should create schema if it doesn't already exist
-#
+#DateTime in flask-sqlalchemy is Python datetime. Conversion to MySQL might be necessary
+class User(db.Model):
+	userid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	username = db.Column(db.String(80), unique=False, nullable=False)
+	password = db.Column(db.String(16), unique=False, nullable=False)
+	email = db.Column(db.String(80), unique=False, nullable=False)
+	createdDate = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+	lastUpdated = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
-class Person(db.Model):
-	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	name = db.Column(db.String(80), unique=False, nullable=False)
+class Profile(db.Model):
+	profileid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	firstname = db.Column(db.String(80), unique=False, nullable=False)
+	lastname = db.Column(db.String(80), unique=False, nullable=False)
+	ssn = db.Column(db.String(9), unique=False, nullable=False)
+	phonenumber = db.Column(db.String(10), unique=False, nullable=False)
+	birthdate = db.Column(db.DateTime, unique=False, nullable=False)
+	createdDate = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+	lastUpdated = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+	userid = db.Column(db.Integer, db.ForeignKey('user.userid'), nullable=False)
+	insuranceid = db.Column(db.Integer, db.ForeignKey('insurance.insuranceid'), nullable=False)
+	insurancecompany = db.relationship('Insurace', backref='Profile', lazy=True)
+	groupnumber = db.relationship('Insurace', backref='Profile', lazy=True)
+	memberid = db.relationship('Insurace', backref='Profile', lazy=True)
 
-@app.route('/')
-def hello():
-	print('Hello world!')
-	return 'Hello world!'
+class Appointment(db.Model):
+	appointmentid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	apptTime = db.Column(db.DateTime, nullable=True)
+	available = db.Column(db.Boolean, nullable=False, default=True)
+	'''
+	month
+	day
+	year
+	time
+	amOrPm
+	'''
+	createdDate = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+	lastUpdated = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+	userid = db.Column(db.Integer, db.ForeignKey('user.userid'), nullable=True)
 
-@app.route('/nothing')
-def handleNothing():
-	return 'Nothing!'
+class Insurance(db.Model):
+	insuranceid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	insurancecompany = db.Column(db.String(80), unique=False, nullable=False)
+	groupnumber = db.Column(db.Integer, nullable=False)
+	memberid = db.Column(db.String(10), unique=True, nullable=False)
+	createdDate = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+	lastUpdated = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
-@app.route('/signup', methods = ['GET', 'POST'])
-def signup():
-	if request.method == 'POST':
-		json_data = request.get_json()
-		addData = Person(name=json_data)
-		db.session.add(addData)
-		db.session.commit()
+db.create_all()
+
+@app.route('/user/<user_id>', methods=['GET, POST, DELETE'])
+def user(user_id):
+	if request.method == 'GET':
+		#return data
+		print(user_id)
+	# else if request.method == 'POST':
+	# 	#modify
+	# elif request.method == 'DELETE':
+	# 	#delete
+	# else:
+	# 	#Error 405 Method Not Allowed
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
